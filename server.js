543478ff8032cfdcba17bec14efe99f8dfba72c9
worker <worker@bergwerk>
2011-11-24 17:32:07 +0100

    onymize anonymous functions

--- server.js
+++ server.js
@@ -2,26 +2,29 @@
 
 var port = 8000;
 
-var root_path = (function () {
+var root_path = (function _init_root_path () {
   var fs = require('fs');
   var path = require('path');
   return path.dirname(fs.realpathSync(__filename));
 })();
 
-
-var handle_externally = (function () {
+var handle_externally = (function _init_handle_externally () {
 
   var spawn = require('child_process').spawn;
   var path = require('path');
 
-  return function (req, res, options, callback) {
+  return function _impl_handle_externally (req, res, options, callback) {
+
+    // the external handler may run a long time... don't time out.
+    res.socket.setTimeout(0 * 60 * 1000);
+
     var log = options.log;
     var env = JSON.parse(JSON.stringify(options.env)); // copy
 
     env.METHOD = req.method;
     env.URL = req.url;
 
-    var child = (function () {
+    var child = (function _init_child () {
       var command = path.join(root_path, 'server.sh');
       var spawn_options = {
         cwd: root_path,
@@ -36,19 +39,19 @@ var handle_externally = (function () {
     // TODO minimal buffering
     var data = [];
 
-    child.stdout.on('data', function (chunk) {
+    child.stdout.on('data', function _cb_buffer_res_chunk (chunk) {
       data.push(chunk);
     });
     
-    child.stderr.on('data', function (chunk) {
+    child.stderr.on('data', function _cb_write_child_stderr (chunk) {
       log.raw(chunk.toString(), '[31m');
     });
     
-    child.on('exit', function (code) {
+    child.on('exit', function _cb_child_exited (code) {
       log.format('child', 'exit', code);
       if (code === 0) {
         // TODO do we have a use case to not generate the whole HTTP response?
-        data.forEach(function (chunk) {
+        data.forEach(function _cb_write_res_chunks (chunk) {
           res.socket.write(chunk);
         });
         res.socket.end();
@@ -65,10 +68,6 @@ var handle_externally = (function () {
 })();
 
 function listener (req, res) {
-
-  // don't time out...
-  res.socket.setTimeout(0 * 60 * 1000);
-
   var log = require('./log').create();
 
   log.raw([req.method, req.url, 'HTTP/' + req.httpVersion].join(' '));
@@ -79,15 +78,15 @@ function listener (req, res) {
 
     form.parse(req, function(err, fields, files) {
 
-      (function pretty_print_form () {
+      (function _scope_pretty_print_form () {
         var pretty = {};
-        Object.keys(fields).forEach(function (key) {
+        Object.keys(fields).forEach(function _cb_cpy_fields (key) {
           pretty[key] = fields[key];
         });
-        Object.keys(files).forEach(function (key) {
+        Object.keys(files).forEach(function _cb_cpy_partial_files (key) {
           pretty[key] = {};
           [ 'name', 'size', 'type', 'path'
-          ].forEach(function (property) {
+          ].forEach(function _cb_cpy_some_file_props (property) {
             pretty[key][property] = files[key][property];
           });
         });
@@ -96,20 +95,24 @@ function listener (req, res) {
 
       // TODO whitelist fields and files?
       var env = {};
-      Object.keys(fields).forEach(function (key) {
+      Object.keys(fields).forEach(function _cb_export_fields (key) {
         env['TEXT_' + key] = fields[key];
       });
-      Object.keys(files).forEach(function (key) {
+      Object.keys(files).forEach(function _cb_export_files (key) {
         env['FILE_' + key] = files[key].path;
         env['FILE_' + key + '_SIZE'] = files[key].size;
         env['FILE_' + key + '_TYPE'] = files[key].type;
       });
 
-      handle_externally(req, res, { log: log, env: env }, function () {
+      var options = {
+        log: log,
+        env: env
+      };
+      handle_externally(req, res, options, function _cb_multipart_cleanup () {
         var fs = require('fs');
-        Object.keys(files).forEach(function (key) {
+        Object.keys(files).forEach(function _cb_unlink_files (key) {
           var file = files[key];
-          fs.unlink(file.path, function (exn) {
+          fs.unlink(file.path, function _cb_file_unlinked (exn) {
             log.format('unlink', file.path, exn);
           });
         });
@@ -117,13 +120,15 @@ function listener (req, res) {
     });
 
   } else {
-    handle_externally(req, res, { log: log, env: {} }, function () {
-      // nop
-    });
+    var options = {
+      log: log,
+      env: {}
+    };
+    handle_externally(req, res, options, function _cb_nop () {});
   };
 };
 
-(function () {
+(function _scope_setup_https_server () {
   var https = require('https');
   var fs = require('fs');
   var path = require('path');
@@ -141,19 +146,17 @@ function listener (req, res) {
     cert: fs.readFileSync(cert_path)
   };
 
-  https.createServer(options, listener).listen(port, function () {
+  https.createServer(options, listener).listen(port, function _cb_motd () {
     console.log('Serving HTTPS on 0.0.0.0 port', port);
   });
 })();
 
-// suicide facility
-(function () {
-  process.on('SIGTERM', function () {
+(function _scope_setup_suicide_facility () {
+  process.on('SIGTERM', function _cb_sigterm () {
     console.log('Got SIGTERM ==> shutting down...');
     process.exit(0);
   });
   var fs = require('fs');
-  //var self = dirname(process.argv[1]);
   var Inotify = require('inotify').Inotify;
   var inotify = new Inotify();
   inotify.addWatch({
@@ -163,7 +166,7 @@ function listener (req, res) {
              | Inotify.IN_MODIFY
              | Inotify.IN_MOVE_SELF
              ,
-    callback: function (event) {
+    callback: function _cb_hara_kiri (event) {
       process.kill(process.pid, 'SIGTERM');
     }
   });
