4bc25b9e30316eff26d44469cfebe2f883fb4b89
worker <worker@bergwerk>
2011-11-24 14:14:03 +0100

    abstact spawnage

--- server.js
+++ server.js
@@ -8,7 +8,87 @@ var root_path = (function () {
   return path.dirname(fs.realpathSync(__filename));
 })();
 
+
+var handle_externally = (function () {
+
+  return function (req, res, log, callback) {
+    // TODO setup environment
+
+    var options = {
+      env: {}, //JSON.parse(JSON.stringify(process.env)),
+      //customFds: [-1, -1, 2]
+    };
+
+    options.env.METHOD = req.method;
+    options.env.URL = req.url;
+
+    var spawn = require('child_process').spawn;
+    var path = require('path');
+    var child  = spawn(path.join(root_path, 'server.sh'), [], options);
+
+    var data = '';
+
+    child.stdout.on('data', function (chunk) {
+      data += chunk;
+    });
+    
+    child.stderr.on('data', function (chunk) {
+      log.raw(chunk.toString(), '[31m');
+    });
+    
+    child.on('exit', function (code) {
+      log.format('child', 'exit', code);
+      if (code === 0) {
+        res.socket.end(data);
+        //var content = data;
+        //res.writeHead(200, {
+        //  'Content-Type': 'text/plain',
+        //  'Content-Length': content.length
+        //});
+        //res.end(content);
+      } else {
+        res.writeHead(500, {
+          'Content-Length': 0
+        });
+        res.end();
+      };
+
+      callback();
+    });
+
+    //bufs: var data = [];
+    //bufs: 
+    //bufs: child.stdout.on('data', function (chunk) {
+    //bufs:   data.push(chunk);
+    //bufs: });
+    //bufs: 
+    //bufs: child.stderr.on('data', function (chunk) {
+    //bufs:   console.log('child stderr: ' + chunk.toString().replace(/\n$/,''));
+    //bufs: });
+    //bufs: 
+    //bufs: child.on('exit', function (code) {
+    //bufs:   if (code === 0) {
+    //bufs:     var content = join_buffers(data);
+    //bufs:     res.writeHead(200, {
+    //bufs:       'Content-Type': 'text/plain',
+    //bufs:       'Content-Length': content.length
+    //bufs:     });
+    //bufs:     res.end(content);
+    //bufs:   } else {
+    //bufs:     console.error('child exit:', code);
+    //bufs:     res.writeHead(500, {
+    //bufs:       'Content-Length': 0
+    //bufs:     });
+    //bufs:     res.end();
+    //bufs:   };
+    //bufs: });
+  };
+})();
+
 function listener (req, res) {
+
+  // don't time out...
+  res.socket.setTimeout(0 * 60 * 1000);
 
   var log = require('./log').create();
 
@@ -26,54 +106,8 @@ function listener (req, res) {
       log.format(files);
     });
   };
-    
 
-
-  // don't time out...
-  res.socket.setTimeout(0 * 60 * 1000);
-
-
-  // TODO setup environment
-
-  var options = {
-    env: {}, //JSON.parse(JSON.stringify(process.env)),
-    //customFds: [-1, -1, 2]
-  };
-
-  options.env.METHOD = req.method;
-  options.env.URL = req.url;
-
-  var spawn = require('child_process').spawn;
-  var path = require('path');
-  var child  = spawn(path.join(root_path, 'server.sh'), [], options);
-
-  var data = '';
-
-  child.stdout.on('data', function (chunk) {
-    data += chunk;
-  });
-  
-  child.stderr.on('data', function (chunk) {
-    log.raw(chunk.toString(), '[31m');
-  });
-  
-  child.on('exit', function (code) {
-    log.format('child', 'exit', code);
-    if (code === 0) {
-      res.socket.end(data);
-      //var content = data;
-      //res.writeHead(200, {
-      //  'Content-Type': 'text/plain',
-      //  'Content-Length': content.length
-      //});
-      //res.end(content);
-    } else {
-      res.writeHead(500, {
-        'Content-Length': 0
-      });
-      res.end();
-    };
-
+  handle_externally(req, res, log, function () {
     var fs = require('fs');
     Object.keys(form_files).forEach(function (key) {
       var file = form_files[key];
@@ -81,35 +115,8 @@ function listener (req, res) {
         log.format('unlink', file.path, exn);
       });
     });
-
   });
 
-  //bufs: var data = [];
-  //bufs: 
-  //bufs: child.stdout.on('data', function (chunk) {
-  //bufs:   data.push(chunk);
-  //bufs: });
-  //bufs: 
-  //bufs: child.stderr.on('data', function (chunk) {
-  //bufs:   console.log('child stderr: ' + chunk.toString().replace(/\n$/,''));
-  //bufs: });
-  //bufs: 
-  //bufs: child.on('exit', function (code) {
-  //bufs:   if (code === 0) {
-  //bufs:     var content = join_buffers(data);
-  //bufs:     res.writeHead(200, {
-  //bufs:       'Content-Type': 'text/plain',
-  //bufs:       'Content-Length': content.length
-  //bufs:     });
-  //bufs:     res.end(content);
-  //bufs:   } else {
-  //bufs:     console.error('child exit:', code);
-  //bufs:     res.writeHead(500, {
-  //bufs:       'Content-Length': 0
-  //bufs:     });
-  //bufs:     res.end();
-  //bufs:   };
-  //bufs: });
 };
 
 (function () {
